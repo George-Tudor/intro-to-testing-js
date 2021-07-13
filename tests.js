d2589f13130c6091078ac4f364dd8c34ef2d085d
George Tudor <Alexander.George.Tudor@gmail.com>
2021-07-13 15:47:40 -0500

    add the second test check for a return string

--- tests.js
+++ tests.js
@@ -17,5 +17,8 @@ describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello()).toBe("string");
     });
-
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello')
 })
