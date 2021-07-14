2989bcea2a47e7049bd83e14964d4024cf5c6221
George Tudor <Alexander.George.Tudor@gmail.com>
2021-07-13 22:51:21 -0500

    Halfway through exercise #8

--- code.js
+++ code.js
@@ -6,8 +6,17 @@ var helloWorld = function() {
 }
 
 function sayHello(name) {
-    return "Hello, " + name + "!";
+    if(name === undefined) {
+        return "Hello, World!";
+    } else if (name === true) {
+        return "Hello, World!";
+    } else if (name === false) {
+        return "Hello, World!";
+    } else {
+        return "Hello, " + name + "!";
+    }
 }
+
 
 // function sayHello(name) {
 //     if (name === "Alex") {
